ea5aba5116af1073ad5ed090e69151f302d8f43e
Mediasoft <99335144+Mediasoftsenegal@users.noreply.github.com>
2024-12-24 12:26:54 +0000

    Delete e_kalaye directory

--- e_kalaye/store/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Categorie, Produit, Panier
-
-# Register your models here.
-admin.site.register(Categorie)
-admin.site.register(Produit)
-admin.site.register(Panier)
--- e_kalaye/store/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from . import views
-
-urlpatterns = [
-    path('', views.list_produit, name='produit_list'),
-    path('produits/<int:produit_id>/', views.produit_detail, name ='produit_detail'),
-    path('panier/',views.panier, name='panier'),
-]
-
